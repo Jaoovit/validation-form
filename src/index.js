6696e0dd2e835675bd83beeaf72724f3cb30ce87
João Oliveira <jaoovit@gmail.com>
2024-04-25 20:22:21 +0100

    add a input and a input:invalid style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./style.css";
+
+const submitBtn = document.querySelector("#submitBtn");
+const email = document.querySelector("#email");
+const emailSpan = document.querySelector("#emailSpan");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+});
