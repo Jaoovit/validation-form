5357d2f45770880e08515dc6b14b79fa007650e7
João Oliveira <jaoovit@gmail.com>
2024-04-26 10:56:35 +0100

    create a rule to validate the email

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import "./style.css";
 
+// Email validation
+
 const submitBtn = document.querySelector("#submitBtn");
 const email = document.querySelector("#email");
 const emailSpan = document.querySelector("#emailSpan");
+const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault;
+function validateEmail() {
+  let emailValidaty = emailPattern.test(email.value);
+  if (email.value.length === 0) {
+    emailSpan.textContent = "This field is required";
+    email.className = "invalid";
+  } else if (emailValidaty == false) {
+    emailSpan.textContent = "Enter an email with a valid format";
+    email.className = "invalid";
+  } else {
+    emailSpan.textContent = "";
+    email.className = "";
+  }
+}
+
+submitBtn.addEventListener("click", () => {
+  validateEmail();
 });
